5f50283bd294b0421f3bd3346e8c20f8f2431ff9
chen.s.g <csg800@126.com>
2024-04-21 11:09:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# 基于书生浦语1.8B的自我认知实验
+
+本实验是学习用 XTuner 微调 internlm2-chat-1-8b 模型，调整模型自我认识的实验过程。
+
+相关资源：
+> - [XTuner](https://github.com/InternLM/xtuner): An efficient, flexible and full-featured toolkit for fine-tuning large models (InternLM, Llama, Baichuan, Qwen, ChatGLM)
+> - [internlm2-chat-1-8b](https://openxlab.org.cn/models/detail/OpenLMLab/internlm2-chat-1.8b): 书生·浦语-1.8B (InternLM2-1.8B) 是第二代浦语模型系列的18亿参数版本
+> - [学习笔记](https://github.com/csg2008/InternLMAgricultureAssistant/blob/main/note/lesson4.md): XTuner 微调 LLM：1.8B、多模态、Agent
---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+
+import gradio as gr
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+base_path = './internlm2-chat-1-8b'
+
+if not os.path.exists(base_path):
+    os.system(f'git clone https://code.openxlab.org.cn/csg2008/internlm2_chat_1_8b_demo.git {base_path}')
+    os.system(f'cd {base_path} && git lfs pull')
+
+title = '基于书生浦语1.8B的自我认知实验'
+description = '微调 internlm2-chat-1-8b 模型调整自我认识的实验，课程笔记: https://github.com/csg2008/InternLMAgricultureAssistant/blob/main/note/lesson4.md'
+tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+
+def chat(message,history):
+    for response, history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+        yield response
+
+gr.ChatInterface(chat, title=title, description=description).queue(1).launch()
